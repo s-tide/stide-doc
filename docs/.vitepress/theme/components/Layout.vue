3ed07cc7212ec07ede5b139b3c23358150b1c554
Jiashun Wang <1043210554@qq.com>
2025-06-16 22:59:24 +0800

    add vercount

---
+++ docs/.vitepress/theme/components/Layout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import VisitData from './VisitData.vue'
+</script>
+
+<template>
+  <template #home-hero-actions-after>
+	<VisitData />
+  </template>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
@@ -15,3 +16,16 @@ export default {
     // ...
   }
 } satisfies Theme
+
+import useVisitData from './hooks/useVisitData'
+
+async enhanceApp({ app, router, siteData }: EnhanceAppContext) {
+  // ...
+  if (inBrowser) {
+    // 网站访问量统计，路由加载完成，在加载页面组件后（在更新页面组件之前）调用。
+    router.onAfterPageLoad = (to: string) => {
+      useVisitData()
+    }
+  }
+  // ...
+}
